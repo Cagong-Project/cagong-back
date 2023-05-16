3fa06652750fa316c8518c4883dd2ed833089750
haram8009 <haram8009@gmail.com>
2023-05-17 02:06:27 +0900

    유저모델 수정 + jwt 도입

--- user/serializers.py
+++ user/serializers.py
@@ -1,6 +1,6 @@
 from .models import *
 from rest_framework import serializers
-
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 class UserSerializer(serializers.ModelSerializer):    
     class Meta:
@@ -19,7 +19,28 @@ class SignupSerializer(serializers.ModelSerializer):
             username=validated_data['username'],
             password=validated_data['password'],
             phone=validated_data['phone'],
-            type=validated_data['type'],
-            #TODO: follower 추가
+            # type=validated_data['type'],
         )
         return user
+    
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    
+    # response 커스텀 
+    default_error_messages = {
+        'no_active_account': {'message':'username or password is incorrect!',
+                              'success': False,
+                              'status' : 401}
+    }
+    # 유효성 검사
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        
+        refresh = self.get_token(self.user)
+        
+         # response에 추가하고 싶은 key값들 추가
+        data['user_id'] = self.user.user_id
+        data['refresh'] = str(refresh)
+        data['access'] = str(refresh.access_token)
+        data['success'] = True
+        
+        return data
--- user/views.py
+++ user/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-
+from rest_framework import permissions
 from .models import User
-from .serializers import UserSerializer, SignupSerializer
+from .serializers import UserSerializer, SignupSerializer, MyTokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from django.contrib.auth.hashers import check_password
+# from rest_framework.views import APIView
 
 
 @api_view(['GET', 'POST'])
@@ -21,3 +25,8 @@ def signup(request):
             serializer.save()
             return Response(status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = MyTokenObtainPairSerializer
