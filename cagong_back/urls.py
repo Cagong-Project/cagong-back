9133aba08fee5385397f6ad5e26cf5f796513a5d
chanwoo7 <chanwoo9987@gmail.com>
2023-05-23 15:42:35 +0900

    webpush.py 삭제하고 send_push 함수를 views.py에 구현, urls.py 수정

--- cagong_back/urls.py
+++ cagong_back/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
-from django.urls import include, path, re_path
+from django.urls import include, path
 import user.views as user_views
 from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
+from .views import home, send_push
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/signup/', user_views.signup),
-    re_path(r'^webpush/', include('webpush.urls')),
+    path('send_push', send_push),
+    path('webpush/', include('webpush.urls')),
     path('api/token/', user_views.MyTokenObtainPairView.as_view(),
          name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(),
