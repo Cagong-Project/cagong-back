99e40c873529ecd36b556e0efe50531e6a3dfea3
chanwoo7 <chanwoo9987@gmail.com>
2023-05-27 00:10:25 +0900

    PushNotification 모델 및 serializers.py 생성

--- pushQue/models.py
+++ pushQue/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class PushNotification(models.Model):
+    title = models.CharField(max_length=100)
+    msg = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)  # 푸시 알림 생성 시각
