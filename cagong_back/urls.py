1ecb59cf323132e71f5e704ff5010b5aee63aa5d
chanwoo7 <chanwoo9987@gmail.com>
2023-05-26 23:59:11 +0900

    views.py 및 urls.py 내의 기존 push 관련 매핑 제거, startapp pushQue

--- cagong_back/urls.py
+++ cagong_back/urls.py
@@ -1,14 +1,11 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 import user.views as user_views
 from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
-from .views import send_push
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/signup/', user_views.signup),
-    path('send_push', send_push),
-    path('webpush/', include('webpush.urls')),
     path('api/token/', user_views.MyTokenObtainPairView.as_view(),
          name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(),
