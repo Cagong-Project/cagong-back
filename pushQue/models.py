a7de2b66d891d629c7db4434f1197e92e4ebfbf4
haram8009 <haram8009@gmail.com>
2023-05-27 15:17:49 +0900

    fix: 오타

--- pushQue/models.py
+++ pushQue/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from user.models import User
 
 class PushNotification(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCAD, verbose_name="수신할 유저")  # 어떤 유저에게 보내야 할 알림인지 지정
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="수신할 유저", null=True)  # 어떤 유저에게 보내야 할 알림인지 지정
     title = models.CharField(max_length=100)
     msg = models.TextField()
     datetime = models.DateTimeField(auto_now_add=True)  # 푸시 알림 생성 시각
