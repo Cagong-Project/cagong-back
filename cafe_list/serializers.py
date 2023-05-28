2a3e297d4e7dea108f38ef6742eb2163d8bc26be
hyeesw <hyeeun7904@kookmin.ac.kr>
2023-05-28 23:08:40 +0900

    CafeDetail 페이지에 cafe_detail 과 menu_list 를 response로 전달 성공

--- cafe_list/serializers.py
+++ cafe_list/serializers.py
@@ -1,11 +1,19 @@
 from .models import *
 from rest_framework import serializers
+from user.serializers import UserSerializer
 
 class CafeSerializer(serializers.ModelSerializer):    
+    name = serializers.CharField()
+    location = serializers.CharField()
+    info = serializers.CharField()
+    phone = serializers.CharField()
+    owner = UserSerializer(read_only=True)
+
     class Meta:
         model = Cafe
         fields = "__all__"
         
+
 class MenuSerializer(serializers.ModelSerializer):
     name = serializers.CharField()
     price = serializers.CharField()
--- cafe_list/views.py
+++ cafe_list/views.py
@@ -3,10 +3,9 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
-import json
 from .models import Cafe, Menu
-from .serializers import MenuSerializer
-
+from .serializers import CafeSerializer, MenuSerializer
+import json
 @api_view(['GET', 'POST'])
 @permission_classes([AllowAny])
 def cafelist(request):
@@ -21,12 +20,6 @@ def cafelist(request):
  
         # 검색 결과 있음
         getCafeList = Cafe.cafeToDictionary(cafeQuerySet) #dictionary 형태
-
-        print("----------------------------------------------------^^^^^^^^^^^")
-        print("엥?",cafeQuerySet)
-        print("----------------------------------------------------^^^^^^^^^^^")
-        print(search_value, getCafeList, type(getCafeList))
-
         result = json.dumps(getCafeList)
         print("json 결과는", result, type(result))
         return Response({"message": "검색 성공!!!", "search_result": result}, status=status.HTTP_200_OK)
@@ -35,12 +28,15 @@ def cafelist(request):
 @api_view(['GET', 'POST'])
 @permission_classes([AllowAny])
 def menu(request, cafe_id):
+    cafe = Cafe.objects.get(id=cafe_id)
+    cafe_dict = {"name" : cafe.name, "location": cafe.location, "info": cafe.info, "phone": cafe.phone}
+    cafe_json = json.dumps(cafe_dict)
+
     menu = list(Menu.objects.filter(cafe = cafe_id).values())
-    serializer = MenuSerializer(data=menu, many= True)
-    print(menu)
-    print("결과를 보여주세요!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    serializer_menu = MenuSerializer(data=menu, many= True)
+
     # 시리얼라이저로 front에게 전송
-    if serializer.is_valid():
-        return Response({"message": "성공!!", "menu_list": serializer.data}, status=status.HTTP_200_OK)
-    print(serializer.errors)
+    if serializer_menu.is_valid():
+        return Response({"message": "성공!!", "cafe_detail": cafe_json, "menu_list": serializer_menu.data}, status=status.HTTP_200_OK)
+    print(serializer_menu.errors)
     return Response({"message": "유효하지않은 serializer"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
