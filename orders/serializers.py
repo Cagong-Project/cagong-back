69f9bae7f073530abb828e68fd79e6f82a182239
hyeesw <hyeeun7904@kookmin.ac.kr>
2023-06-18 18:17:47 +0900

    order 시리얼라이저 수정

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,20 +1,20 @@
 from rest_framework import serializers
 from .models import *
 
-class CafeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cafe
-        fields = '__all__'
+# class CafeSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Cafe
+#         fields = '__all__'
         
-class MenuSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Menu
-        fields = '__all__'
+# class MenuSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Menu
+#         fields = '__all__'
 
-class PointSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Point
-        fields = '__all__'
+# class PointSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Point
+#         fields = '__all__'
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
