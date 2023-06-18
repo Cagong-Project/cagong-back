a8e2bebf94f1fe61f9cbc9f657a0ef00b948ba39
jm3789 <jm37890@gmail.com>
2023-06-19 00:41:39 +0900

    #20 405에러: detail이 post 요청을 detail2로 받음(에러해결실패)

--- records/urls.py
+++ records/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('detail/<int:record_id>', views.detail, name='detail'),
+    path('detail2/<int:record_id>', views.detail2, name='detail2'),
     path('record_list/<str:user_id>',
          views.record_list, name='record_list'),
 ]
--- records/views.py
+++ records/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
@@ -8,6 +10,7 @@ from .serializers import RecordSerializer
 
 
 @api_view(['GET'])
+@permission_classes([AllowAny])
 def detail(request, record_id):
     record = Record.objects.filter(id=record_id).first
     # serializer = RecordSerializer(data=record)
@@ -15,6 +18,18 @@ def detail(request, record_id):
     if record is not None:
         return Response({"message": "a record obj", "record":record}, status=status.HTTP_200_OK)
     return Response({"message": "레코드가 없습니다"}, status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def detail2(request, record_id):    
+    record = Record.objects.filter(id=record_id).first
+    if record is not None:
+        #record.end = timezone.now()  # 현재 시각으로 설정
+        #record.duration = record.end - record.start  # duration 계산
+        record.save()
+        serializer = RecordSerializer(record)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response({"message": "레코드가 없습니다."}, status=status.HTTP_404_NOT_FOUND)
 
 
 @api_view(['GET'])
