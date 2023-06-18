9453d3682a61495ab321f91a4c8f98d37f33f85c
haram8009 <haram8009@gmail.com>
2023-06-19 01:27:55 +0900

    resolve #20 레코드 종료 기능 완성

--- records/urls.py
+++ records/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('detail/<int:record_id>', views.detail, name='detail'),
-    path('detail2/<int:record_id>', views.detail2, name='detail2'),
+    path('done', views.done, name='done'),
     path('record_list/<str:user_id>',
          views.record_list, name='record_list'),
 ]
--- records/views.py
+++ records/views.py
@@ -7,28 +7,28 @@ from rest_framework import status
 from .models import Record
 from .serializers import RecordSerializer
 # Create your views here.
+from django.utils import timezone
 
 
 @api_view(['GET'])
-@permission_classes([AllowAny])
 def detail(request, record_id):
-    record = Record.objects.filter(id=record_id).first
+    record = Record.objects.filter(id=record_id).first()
     # serializer = RecordSerializer(data=record)
     # if serializer.is_valid():
     if record is not None:
         return Response({"message": "a record obj", "record":record}, status=status.HTTP_200_OK)
     return Response({"message": "레코드가 없습니다"}, status=status.HTTP_404_NOT_FOUND)
 
-@api_view(['POST'])
-@permission_classes([AllowAny])
-def detail2(request, record_id):    
-    record = Record.objects.filter(id=record_id).first
+@api_view(['PUT'])
+def done(request):
+    print(request.data)
+    record = Record.objects.filter(id=request.data['record_id']).first()
+    print(record)
     if record is not None:
-        #record.end = timezone.now()  # 현재 시각으로 설정
+        record.end = timezone.now()  # 현재 시각으로 설정
         #record.duration = record.end - record.start  # duration 계산
         record.save()
-        serializer = RecordSerializer(record)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({"message":"레코드가 업데이트 되었습니다."}, status=status.HTTP_200_OK)
     return Response({"message": "레코드가 없습니다."}, status=status.HTTP_404_NOT_FOUND)
 
 
